7685e44a48a63b219e0b57f069a0159046e5dcd6
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-09 16:19:52 +0200

    Corrected and improved basic-pagination (and removed modifier).

--- src/sv/reagent/component/basic_pagination.cljs
+++ src/sv/reagent/component/basic_pagination.cljs
@@ -25,35 +25,58 @@
     {:start start
      :end end}))
 
-(defn pagination [modifier page-change]
-  (let [v (modifier)]
-    (if-not (:page v)
-      [:div ""]
-      [:div
-       [a "First " (fn [e]
-                     (go
-                       (<! (page-change 0))
-                       (modifier assoc :page 0)))]
-       [a "< Prev" (fn [e]
-                     (go
-                       (when (< 0 (:page v))
-                         (<! (page-change (dec (:page v))))
-                         (modifier update-in [:page] dec))))]
-       (let [{:keys [start end]} (page-row-idxs v)]
-         [:span " Rows "
-          start " - " end
-          " of " (:page-count v) " "])
-       [a "Next >" (fn [e]
-                     (go
-                       (when (> (:page-count v)
-                                (let [page-size (:page-size v default-page-size)]
-                                  (* (inc (:page v)) page-size)))
-                         (<! (page-change (inc (:page v))))
-                         (modifier update-in [:page] inc))))]
-       [a " Last" (fn [e]
-                    (go
-                      (let [page (long (/ (:page-count v)
-                                          (:page-size v default-page-size)))]
-                        (<! (page-change page))
-                        (modifier assoc :page page))))]])))
+(defn view [params]
+  (let [{:keys [page page-size count]} params
+        start-row (* page page-size)
+        last-page (dec (.ceil js/Math
+                              (/ count
+                                 page-size)))]
+    {:page page
+     :page-size page-size
+     :count count
+     :last-page last-page
+     :start-row start-row
+     :end-row (let [end-row (+ start-row page-size)]
+                (if (< end-row count)
+                  end-row
+                  count))
+     :next (not= page last-page)
+     :prev (not= page 0)}))
 
+(def defaults
+  {:page-size default-page-size})
+
+(defn pagination [model page-change]
+  (let [m @model]
+    [:div
+     (when (:page m)
+       (let [v (view (merge defaults m))]
+         [:div
+          (when (:prev v)
+            [a "First " (fn [e]
+                          (go
+                            (<! (page-change 0))
+                            (swap! model assoc :page 0)))])
+          (when (:prev v)
+            [a "< Prev" (fn [e]
+                          (go
+                            (when (< 0 (:page v))
+                              (let [page (dec (:page v))]
+                                (<! (page-change page))
+                                (swap! model assoc :page page)))))])
+          (let [{:keys [start-row end-row]} v]
+            [:span " Rows "
+             start-row " - " end-row
+             " of " (:count v) " "])
+          (when (:next v)
+            [a "Next >" (fn [e]
+                          (go
+                            (let [page (inc (:page v))]
+                              (<! (page-change page))
+                              (swap! model assoc :page page))))])
+          (when (:next v)
+            [a " Last" (fn [e]
+                         (go
+                           (let [page (:last-page v)]
+                             (<! (page-change page))
+                             (swap! model assoc :page page))))])]))]))
